04bed8ad8c55ea6257f994112f6fd5ddd9357321
Yashima-san <liyashima18@gmail.com>
2025-04-01 11:06:30 +0700

    work_7

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
-  
+
 def index(request):
-    return HttpResponse("Hello METANIT.COM")
+    return HttpResponse("<h2>Главная</h2>")
+  
+def user(request, name="Undefined", age =0):
+    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+def about(request, name, age):
+    return HttpResponse(f"""
+            <h2>О пользователе</h2>
+            <p>Имя: {name}</p>
+            <p>Возраст: {age}</p>
+    """)
+ 
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
